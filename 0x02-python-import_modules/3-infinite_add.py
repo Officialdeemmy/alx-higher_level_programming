41c9f5523e527d84f56e029adbbc70dd6012b9da
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-11-30 03:11:07 -0800

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-def infinite_add():
     sum = 0
 
     for i in range(len(sys.argv) - 1):
         sum += int(sys.argv[i + 1])
     print("{}".format(sum))
-
-
-if __name__ == "__main__":
-    infinite_add()
