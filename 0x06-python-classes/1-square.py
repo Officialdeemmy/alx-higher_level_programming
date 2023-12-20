0c443b86bd26e861884f352e98ae7e2acf9bcc79
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-12-20 13:51:22 -0800

    Object-Oriented Programming

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define square """
+
+
+class Square:
+    """ defines function called __init__ """
+    def __init__(self, size):
+        """ initializes __size of self with size """
+        self.__size = size
